65941103ccb5e9358132480ffa10535a01fdd1bb
John Ryan <john.p.ryan4@gmail.com>
2018-03-07 08:24:47 -0800

    cleanup polyline layer

--- flutter_map/lib/src/layer/polyline_layer.dart
+++ flutter_map/lib/src/layer/polyline_layer.dart
@@ -3,7 +3,6 @@ import 'package:latlong/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'dart:ui';
 
-
 class PolylineLayerOptions extends LayerOptions {
   final List<Polyline> polylines;
   PolylineLayerOptions({this.polylines = const []});
@@ -11,7 +10,7 @@ class PolylineLayerOptions extends LayerOptions {
 
 class Polyline {
   final List<LatLng> points;
-  List<Offset> offsets;
+  final List<Offset> offsets = [];
   final double strokeWidth;
   final Color color;
   Polyline({
@@ -29,32 +28,31 @@ class PolylineLayer extends StatelessWidget {
   Widget build(BuildContext context) {
     return new StreamBuilder<int>(
       stream: map.onMoved, // a Stream<int> or null
-      builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
-        for (var polylineOpt in this.polylineOpts.polylines) {
-          polylineOpt.offsets = [];
+      builder: (BuildContext context, _) {
+        for (var polylineOpt in polylineOpts.polylines) {
+          polylineOpt.offsets.clear();
           var i = 0;
           for (var point in polylineOpt.points) {
-            i++;
             var pos = map.project(point);
             pos = pos.multiplyBy(map.getZoomScale(map.zoom, map.zoom)) -
                 map.getPixelOrigin();
             polylineOpt.offsets.add(new Offset(pos.x, pos.y));
-            if (i != 1 && i != polylineOpt.points.length) {
+            if (i > 0 && i < polylineOpt.points.length) {
               polylineOpt.offsets.add(new Offset(pos.x, pos.y));
             }
+            i++;
           }
         }
+
         var polylines = <Widget>[];
         for (var polylineOpt in this.polylineOpts.polylines) {
           polylines.add(
             new CustomPaint(
-                painter: new PolylinePainter(polylineOpt),
-                child: new Container(
-                  child: new Text(
-                      "                                                                                                   "),
-                )),
+              painter: new PolylinePainter(polylineOpt),
+            ),
           );
         }
+
         return new Container(
           child: new Stack(
             children: polylines,
@@ -71,10 +69,10 @@ class PolylinePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    if (polylineOpt.offsets == null) {
+    if (polylineOpt.offsets.isEmpty) {
       return;
     }
-    final Paint paint = new Paint()..color = polylineOpt.color;
+    var paint = new Paint()..color = polylineOpt.color;
     paint.strokeWidth = polylineOpt.strokeWidth;
     canvas.drawPoints(PointMode.lines, polylineOpt.offsets, paint);
   }
