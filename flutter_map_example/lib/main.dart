8ffad40da6aba76dbac3bb04f7c1bb4c4a87989a
John Ryan <john.p.ryan4@gmail.com>
2018-04-18 10:01:45 -0700

    add example for MapController

--- flutter_map_example/lib/main.dart
+++ flutter_map_example/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         TapToAddPage.route: (context) => new TapToAddPage(),
         EsriPage.route: (context) => new EsriPage(),
         PolylinePage.route: (context) => new PolylinePage(),
+        MapControllerPage.route: (context) => new MapControllerPage(),
       },
     );
   }
@@ -221,9 +222,8 @@ class PolylinePage extends StatelessWidget {
                     polylines: [
                       new Polyline(
                           points: points,
-                        strokeWidth: 4.0,
-                        color: Colors.purple
-                      ),
+                          strokeWidth: 4.0,
+                          color: Colors.purple),
                     ],
                   )
                 ],
@@ -282,9 +282,122 @@ Drawer _buildDrawer(BuildContext context, String currentRoute) {
             Navigator.popAndPushNamed(context, PolylinePage.route);
           },
         ),
+        new ListTile(
+          title: const Text('MapController'),
+          selected: currentRoute == MapControllerPage.route,
+          onTap: () {
+            Navigator.popAndPushNamed(context, MapControllerPage.route);
+          },
+        ),
       ],
     ),
   );
+}
+
+class MapControllerPage extends StatefulWidget {
+  static const String route = 'map_controller';
+
+  @override
+  MapControllerPageState createState() {
+    return new MapControllerPageState();
+  }
+}
+
+class MapControllerPageState extends State<MapControllerPage> {
+  static LatLng london = new LatLng(51.5, -0.09);
+  static LatLng paris = new LatLng(48.8566, 2.3522);
+  static LatLng dublin = new LatLng(53.3498, -6.2603);
+
+  MapController mapController;
+
+  void initState() {
+    super.initState();
+    mapController = new MapController();
+  }
+
+  Widget build(BuildContext context) {
+    var markers = <Marker>[
+      new Marker(
+        width: 80.0,
+        height: 80.0,
+        point: london,
+        builder: (ctx) => new Container(
+              child: new FlutterLogo(),
+            ),
+      ),
+      new Marker(
+        width: 80.0,
+        height: 80.0,
+        point: dublin,
+        builder: (ctx) => new Container(
+              child: new FlutterLogo(
+                colors: Colors.green,
+              ),
+            ),
+      ),
+      new Marker(
+        width: 80.0,
+        height: 80.0,
+        point: paris,
+        builder: (ctx) => new Container(
+              child: new FlutterLogo(colors: Colors.purple),
+            ),
+      ),
+    ];
+
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("MapController")),
+      drawer: _buildDrawer(context, MapControllerPage.route),
+      body: new Padding(
+        padding: new EdgeInsets.all(8.0),
+        child: new Column(
+          children: [
+            new Padding(
+              padding: new EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: new Row(
+                children: <Widget>[
+                  new MaterialButton(
+                    child: new Text("London"),
+                    onPressed: () {
+                      mapController.move(london, 5.0);
+                    },
+                  ),
+                  new MaterialButton(
+                    child: new Text("Paris"),
+                    onPressed: () {
+                      mapController.move(paris, 5.0);
+                    },
+                  ),
+                  new MaterialButton(
+                    child: new Text("Dublin"),
+                    onPressed: () {
+                      mapController.move(dublin, 5.0);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            new Flexible(
+              child: new FlutterMap(
+                mapController: mapController,
+                options: new MapOptions(
+                  center: new LatLng(51.5, -0.09),
+                  zoom: 5.0,
+                ),
+                layers: [
+                  new TileLayerOptions(
+                      urlTemplate:
+                          "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: ['a', 'b', 'c']),
+                  new MarkerLayerOptions(markers: markers)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 // Generated using Material Design Palette/Theme Generator
