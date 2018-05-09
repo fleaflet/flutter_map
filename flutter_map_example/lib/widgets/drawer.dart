6c5ae2a0b09d71a3bad7cee971415a22281a690f
John Ryan <john.p.ryan4@gmail.com>
2018-05-09 14:49:08 -0700

    reorganize example app

---
+++ flutter_map_example/lib/widgets/drawer.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map_example/main.dart';
+import 'package:flutter_map_example/pages/esri.dart';
+import 'package:flutter_map_example/pages/home.dart';
+import 'package:flutter_map_example/pages/map_controller.dart';
+import 'package:flutter_map_example/pages/marker_anchor.dart';
+import 'package:flutter_map_example/pages/polyline.dart';
+import 'package:flutter_map_example/pages/tap_to_add.dart';
+
+Drawer buildDrawer(BuildContext context, String currentRoute) {
+  return new Drawer(
+    child: new ListView(
+      children: <Widget>[
+        const DrawerHeader(
+          child: const Center(
+            child: const Text("Flutter Map Examples"),
+          ),
+        ),
+        new ListTile(
+          title: const Text('OpenStreetMap'),
+          selected: currentRoute == HomePage.route,
+          onTap: () {
+            Navigator.popAndPushNamed(context, HomePage.route);
+          },
+        ),
+        new ListTile(
+          title: const Text('Add Pins'),
+          selected: currentRoute == TapToAddPage.route,
+          onTap: () {
+            Navigator.popAndPushNamed(context, TapToAddPage.route);
+          },
+        ),
+        new ListTile(
+          title: const Text('Esri'),
+          selected: currentRoute == EsriPage.route,
+          onTap: () {
+            Navigator.popAndPushNamed(context, EsriPage.route);
+          },
+        ),
+        new ListTile(
+          title: const Text('Polylines'),
+          selected: currentRoute == EsriPage.route,
+          onTap: () {
+            Navigator.popAndPushNamed(context, PolylinePage.route);
+          },
+        ),
+        new ListTile(
+          title: const Text('MapController'),
+          selected: currentRoute == MapControllerPage.route,
+          onTap: () {
+            Navigator.popAndPushNamed(context, MapControllerPage.route);
+          },
+        ),
+        new ListTile(
+          title: const Text('Marker Anchors'),
+          selected: currentRoute == MarkerAnchorPage.route,
+          onTap: () {
+            Navigator.popAndPushNamed(context, MarkerAnchorPage.route);
+          },
+        ),
+      ],
+    ),
+  );
+}
+
+class MyCustomRoute<T> extends MaterialPageRoute<T> {
+  MyCustomRoute({ WidgetBuilder builder, RouteSettings settings })
+      : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) {
+    if (settings.isInitialRoute)
+      return child;
+    // Fades between routes. (If you don't want any animation,
+    // just return child.)
+    return new FadeTransition(opacity: animation, child: child);
+  }
+}
