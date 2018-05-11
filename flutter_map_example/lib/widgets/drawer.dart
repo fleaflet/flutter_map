bfad94f447ee82c5a185ecbaf6553cfe98c4418b
John Ryan <john.p.ryan4@gmail.com>
2018-05-11 12:22:43 -0700

    remove MyCustomRoute

--- flutter_map_example/lib/widgets/drawer.dart
+++ flutter_map_example/lib/widgets/drawer.dart
@@ -62,20 +62,3 @@ Drawer buildDrawer(BuildContext context, String currentRoute) {
     ),
   );
 }
-
-class MyCustomRoute<T> extends MaterialPageRoute<T> {
-  MyCustomRoute({ WidgetBuilder builder, RouteSettings settings })
-      : super(builder: builder, settings: settings);
-
-  @override
-  Widget buildTransitions(BuildContext context,
-      Animation<double> animation,
-      Animation<double> secondaryAnimation,
-      Widget child) {
-    if (settings.isInitialRoute)
-      return child;
-    // Fades between routes. (If you don't want any animation,
-    // just return child.)
-    return new FadeTransition(opacity: animation, child: child);
-  }
-}
