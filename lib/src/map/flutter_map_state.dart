4a6e515aee690c62df56740a7ffb11b1cd9df2ea
Morten Mortensen <mortensen.morten@gmail.com>
2018-10-19 15:32:13 +0200

    Added circle layer

--- lib/src/map/flutter_map_state.dart
+++ lib/src/map/flutter_map_state.dart
@@ -53,6 +53,9 @@ class FlutterMapState extends MapGestureMixin {
     if (options is PolygonLayerOptions) {
       return new PolygonLayer(options, mapState);
     }
+    if (options is CircleLayerOptions) {
+      return new CircleLayer(options, mapState);
+    }
     for (var plugin in plugins) {
       if (plugin.supportsLayer(options)) {
         return plugin.createLayer(options, mapState);
