7d13c5760dc86d4cbaced3f19c3206c4febfbbab
jinbangzhu <jinbangzhu@gmail.com>
2019-12-15 15:24:59 +0800

    upgrade flutter version, support Flutter 1.12

--- example/android/app/src/main/java/com/johnpryan/leafletflutterexample/MainActivity.java
+++ example/android/app/src/main/java/com/johnpryan/leafletflutterexample/MainActivity.java
@@ -1,14 +1,13 @@
 package com.johnpryan.leafletflutterexample;
 
 import android.os.Bundle;
-
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+  public void configureFlutterEngine(FlutterEngine flutterEngine){
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
 }
