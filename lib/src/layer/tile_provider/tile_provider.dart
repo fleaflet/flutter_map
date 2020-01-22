0eaed5eca4157cbeaf8db6356e7438503dba167c
Dmitriy <bugrov_dmitrij@mail.ru>
2020-01-22 16:20:30 +0300

    hot fix

--- lib/src/layer/tile_provider/tile_provider.dart
+++ lib/src/layer/tile_provider/tile_provider.dart
@@ -4,7 +4,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_image/network.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map/src/core/bounds.dart';
 import 'package:flutter_map/src/core/util.dart' as util;
 
 export 'package:flutter_map/src/layer/tile_provider/mbtiles_image_provider.dart';
