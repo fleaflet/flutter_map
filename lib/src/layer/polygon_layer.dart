da8f45621a6c9235e1f6fa3ea7889cd550fc4dfe
maRci002 <matuzmarci@gmail.com>
2020-02-25 15:33:06 +0100

    Support holed polygon

--- lib/src/layer/polygon_layer.dart
+++ lib/src/layer/polygon_layer.dart
@@ -19,19 +19,26 @@ class PolygonLayerOptions extends LayerOptions {
 class Polygon {
   final List<LatLng> points;
   final List<Offset> offsets = [];
+  final List<List<LatLng>> holePointsList;
+  final List<List<Offset>> holeOffsetsList;
   final Color color;
   final double borderStrokeWidth;
   final Color borderColor;
+  final bool disableHolesBorder;
   final bool isDotted;
   LatLngBounds boundingBox;
 
   Polygon({
     this.points,
+    this.holePointsList,
     this.color = const Color(0xFF00FF00),
     this.borderStrokeWidth = 0.0,
     this.borderColor = const Color(0xFFFFFF00),
+    this.disableHolesBorder = false,
     this.isDotted = false,
-  }) {
+  }) : holeOffsetsList = null == holePointsList
+            ? null
+            : List.generate(holePointsList.length, (_) => []) {
     boundingBox = LatLngBounds.fromPoints(points);
   }
 }
@@ -62,7 +69,12 @@ class PolygonLayer extends StatelessWidget {
 
         for (var polygon in polygonOpts.polygons) {
           polygon.offsets.clear();
-          var i = 0;
+
+          if (null != polygon.holeOffsetsList) {
+            for (var offsets in polygon.holeOffsetsList) {
+              offsets.clear();
+            }
+          }
 
           if (polygonOpts.polygonCulling &&
               !polygon.boundingBox.isOverlapping(map.bounds)) {
@@ -70,15 +82,13 @@ class PolygonLayer extends StatelessWidget {
             continue;
           }
 
-          for (var point in polygon.points) {
-            var pos = map.project(point);
-            pos = pos.multiplyBy(map.getZoomScale(map.zoom, map.zoom)) -
-                map.getPixelOrigin();
-            polygon.offsets.add(Offset(pos.x.toDouble(), pos.y.toDouble()));
-            if (i > 0 && i < polygon.points.length) {
-              polygon.offsets.add(Offset(pos.x.toDouble(), pos.y.toDouble()));
+          _fillOffsets(polygon.offsets, polygon.points);
+
+          if (null != polygon.holePointsList) {
+            for (var i = 0, len = polygon.holePointsList.length; i < len; ++i) {
+              _fillOffsets(
+                  polygon.holeOffsetsList[i], polygon.holePointsList[i]);
             }
-            i++;
           }
 
           polygons.add(
@@ -97,6 +107,20 @@ class PolygonLayer extends StatelessWidget {
       },
     );
   }
+
+  void _fillOffsets(final List<Offset> offsets, final List<LatLng> points) {
+    for (var i = 0, len = points.length; i < len; ++i) {
+      var point = points[i];
+
+      var pos = map.project(point);
+      pos = pos.multiplyBy(map.getZoomScale(map.zoom, map.zoom)) -
+          map.getPixelOrigin();
+      offsets.add(Offset(pos.x.toDouble(), pos.y.toDouble()));
+      if (i > 0) {
+        offsets.add(Offset(pos.x.toDouble(), pos.y.toDouble()));
+      }
+    }
+  }
 }
 
 class PolygonPainter extends CustomPainter {
@@ -110,26 +134,38 @@ class PolygonPainter extends CustomPainter {
       return;
     }
     final rect = Offset.zero & size;
-    canvas.clipRect(rect);
-    final paint = Paint()
-      ..style = PaintingStyle.fill
-      ..color = polygonOpt.color;
-    final borderPaint = polygonOpt.borderStrokeWidth > 0.0
-        ? (Paint()
-          ..color = polygonOpt.borderColor
-          ..strokeWidth = polygonOpt.borderStrokeWidth)
-        : null;
+    _paintPolygon(canvas, rect);
 
-    _paintPolygon(canvas, polygonOpt.offsets, paint);
-
-    var borderRadius = (polygonOpt.borderStrokeWidth / 2);
     if (polygonOpt.borderStrokeWidth > 0.0) {
+      var borderRadius = (polygonOpt.borderStrokeWidth / 2);
+
+      final borderPaint = polygonOpt.borderStrokeWidth > 0.0
+          ? (Paint()
+            ..color = polygonOpt.borderColor
+            ..strokeWidth = polygonOpt.borderStrokeWidth)
+          : null;
+
       if (polygonOpt.isDotted) {
         var spacing = polygonOpt.borderStrokeWidth * 1.5;
         _paintDottedLine(
             canvas, polygonOpt.offsets, borderRadius, spacing, borderPaint);
+
+        if (!polygonOpt.disableHolesBorder &&
+            null != polygonOpt.holeOffsetsList) {
+          for (var offsets in polygonOpt.holeOffsetsList) {
+            _paintDottedLine(
+                canvas, offsets, borderRadius, spacing, borderPaint);
+          }
+        }
       } else {
         _paintLine(canvas, polygonOpt.offsets, borderRadius, borderPaint);
+
+        if (!polygonOpt.disableHolesBorder &&
+            null != polygonOpt.holeOffsetsList) {
+          for (var offsets in polygonOpt.holeOffsetsList) {
+            _paintLine(canvas, offsets, borderRadius, borderPaint);
+          }
+        }
       }
     }
   }
@@ -153,7 +189,7 @@ class PolygonPainter extends CustomPainter {
           ? stepLength - (totalDistance - distance)
           : distance - totalDistance;
     }
-    canvas.drawCircle(polygonOpt.offsets.last, radius, paint);
+    canvas.drawCircle(offsets.last, radius, paint);
   }
 
   void _paintLine(
@@ -164,10 +200,36 @@ class PolygonPainter extends CustomPainter {
     }
   }
 
-  void _paintPolygon(Canvas canvas, List<Offset> offsets, Paint paint) {
-    var path = Path();
-    path.addPolygon(offsets, true);
-    canvas.drawPath(path, paint);
+  void _paintPolygon(Canvas canvas, Rect rect) {
+    if (null != polygonOpt.holeOffsetsList) {
+      canvas.saveLayer(rect, Paint());
+      final paint = Paint()..style = PaintingStyle.fill;
+
+      for (var offsets in polygonOpt.holeOffsetsList) {
+        var path = Path();
+        path.addPolygon(offsets, true);
+        canvas.drawPath(path, paint);
+      }
+
+      paint
+        ..color = polygonOpt.color
+        ..blendMode = BlendMode.srcOut;
+
+      var path = Path();
+      path.addPolygon(polygonOpt.offsets, true);
+      canvas.drawPath(path, paint);
+
+      canvas.restore();
+    } else {
+      canvas.clipRect(rect);
+      final paint = Paint()
+        ..style = PaintingStyle.fill
+        ..color = polygonOpt.color;
+
+      var path = Path();
+      path.addPolygon(polygonOpt.offsets, true);
+      canvas.drawPath(path, paint);
+    }
   }
 
   @override
