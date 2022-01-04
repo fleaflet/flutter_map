457cb61b638385ae379bc4294121914f9a7ce3d6
Hugo Heneault <hugo@ownweb.fr>
2022-01-04 19:36:30 +0100

    Better pointToLatLng example

--- example/lib/pages/point_to_latlng.dart
+++ example/lib/pages/point_to_latlng.dart
@@ -20,12 +20,16 @@ class PointToLatlngPage extends State<PointToLatLngPage> {
   final pointSize = 20.0;
   late final mapEventSubscription;
 
+  LatLng? latLng;
+
   @override
   void initState() {
     super.initState();
     mapController = MapController();
 
     mapEventSubscription = mapController.mapEventStream.listen(onMapEvent);
+
+    mapController.onReady.then((_) => _updatePointLatLng());
   }
 
   @override
@@ -35,21 +39,31 @@ class PointToLatlngPage extends State<PointToLatLngPage> {
       drawer: buildDrawer(context, PointToLatLngPage.route),
       body: Stack(
         children: [
-          FlutterMap(
-            mapController: mapController,
-            options: MapOptions(
-              center: LatLng(51.5, -0.09),
-              zoom: 5.0,
-              maxZoom: 5.0,
-              minZoom: 3.0,
+          Container(
+            child: FlutterMap(
+              mapController: mapController,
+              options: MapOptions(
+                center: LatLng(51.5, -0.09),
+                zoom: 5.0,
+                maxZoom: 5.0,
+                minZoom: 3.0,
+              ),
+              layers: [
+                TileLayerOptions(
+                    urlTemplate:
+                        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    subdomains: ['a', 'b', 'c']),
+              ],
             ),
-            layers: [
-              TileLayerOptions(
-                  urlTemplate:
-                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  subdomains: ['a', 'b', 'c']),
-            ],
           ),
+          Container(
+              color: Colors.white,
+              height: 60,
+              child: Center(
+                  child: Text(
+                'top left of square is hovering ${latLng?.latitude},${latLng?.longitude}',
+                textAlign: TextAlign.center,
+              ))),
           Positioned(
               top: pointY,
               left: pointX,
@@ -62,9 +76,15 @@ class PointToLatlngPage extends State<PointToLatLngPage> {
 
   void onMapEvent(MapEvent mapEvent) {
     if (mapEvent is MapEventMove) {
-      final latLng = mapController.pointToLatLng(CustomPoint(pointX, pointY));
-
-      print('top left of square is hovering $latLng');
+      _updatePointLatLng();
     }
+  }
+
+  void _updatePointLatLng() {
+    final latLng = mapController.pointToLatLng(CustomPoint(pointX, pointY));
+
+    setState(() {
+      this.latLng = latLng;
+    });
   }
 }
