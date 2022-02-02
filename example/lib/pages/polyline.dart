52dc38e9fa0738afe370c9201bba5e0cec13a0a7
jithware <30555367+jithware@users.noreply.github.com>
2022-02-02 08:15:18 -0500

    #1037: fix LateInitializationError with polylineCulling (#1110)

--- example/lib/pages/polyline.dart
+++ example/lib/pages/polyline.dart
@@ -4,8 +4,37 @@ import 'package:latlong2/latlong.dart';
 
 import '../widgets/drawer.dart';
 
-class PolylinePage extends StatelessWidget {
+class PolylinePage extends StatefulWidget {
   static const String route = 'polyline';
+
+  @override
+  State<PolylinePage> createState() => _PolylinePageState();
+}
+
+class _PolylinePageState extends State<PolylinePage> {
+  late Future<List<Polyline>> polylines;
+
+  Future<List<Polyline>> getPolylines() async {
+    var polyLines = [
+      Polyline(
+        points: [
+          LatLng(50.5, -0.09),
+          LatLng(51.3498, -6.2603),
+          LatLng(53.8566, 2.3522),
+        ],
+        strokeWidth: 4.0,
+        color: Colors.amber,
+      ),
+    ];
+    await Future.delayed(Duration(seconds: 3));
+    return polyLines;
+  }
+
+  @override
+  void initState() {
+    polylines = getPolylines();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,54 +51,74 @@ class PolylinePage extends StatelessWidget {
     ];
 
     return Scaffold(
-      appBar: AppBar(title: Text('Polylines')),
-      drawer: buildDrawer(context, PolylinePage.route),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
-              child: Text('Polylines'),
-            ),
-            Flexible(
-              child: FlutterMap(
-                options: MapOptions(
-                  center: LatLng(51.5, -0.09),
-                  zoom: 5.0,
-                ),
-                layers: [
-                  TileLayerOptions(
-                      urlTemplate:
-                          'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                      subdomains: ['a', 'b', 'c']),
-                  PolylineLayerOptions(
-                    polylines: [
-                      Polyline(
-                          points: points,
-                          strokeWidth: 4.0,
-                          color: Colors.purple),
-                    ],
-                  ),
-                  PolylineLayerOptions(
-                    polylines: [
-                      Polyline(
-                        points: pointsGradient,
-                        strokeWidth: 4.0,
-                        gradientColors: [
-                          Color(0xffE40203),
-                          Color(0xffFEED00),
-                          Color(0xff007E2D),
+        appBar: AppBar(title: Text('Polylines')),
+        drawer: buildDrawer(context, PolylinePage.route),
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: FutureBuilder<List<Polyline>>(
+            future: polylines,
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Polyline>> snapshot) {
+              debugPrint('snapshot: ${snapshot.hasData}');
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                      child: Text('Polylines'),
+                    ),
+                    Flexible(
+                      child: FlutterMap(
+                        options: MapOptions(
+                          center: LatLng(51.5, -0.09),
+                          zoom: 5.0,
+                          onTap: (tapPosition, point) {
+                            setState(() {
+                              debugPrint('onTap');
+                              polylines = getPolylines();
+                            });
+                          },
+                        ),
+                        layers: [
+                          TileLayerOptions(
+                              urlTemplate:
+                                  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                              subdomains: ['a', 'b', 'c']),
+                          PolylineLayerOptions(
+                            polylines: [
+                              Polyline(
+                                  points: points,
+                                  strokeWidth: 4.0,
+                                  color: Colors.purple),
+                            ],
+                          ),
+                          PolylineLayerOptions(
+                            polylines: [
+                              Polyline(
+                                points: pointsGradient,
+                                strokeWidth: 4.0,
+                                gradientColors: [
+                                  Color(0xffE40203),
+                                  Color(0xffFEED00),
+                                  Color(0xff007E2D),
+                                ],
+                              ),
+                            ],
+                          ),
+                          PolylineLayerOptions(
+                            polylines: snapshot.data!,
+                            polylineCulling: true,
+                          ),
                         ],
                       ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+                    ),
+                  ],
+                );
+              }
+              return const Text(
+                  'Getting map data...\n\nTap on map when complete to refresh map data.');
+            },
+          ),
+        ));
   }
 }
