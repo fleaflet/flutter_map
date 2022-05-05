29d26d688a725acc72c029c15a3fc5d927ebb379
André Sousa <andrelvsousa@gmail.com>
2022-05-05 17:34:02 -0300

    Add polygon label placement options

--- lib/src/layer/label.dart
+++ lib/src/layer/label.dart
@@ -1,26 +1,30 @@
-import 'dart:math';
+import 'dart:math' as math;
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:polylabel/polylabel.dart';
 
 class Label {
   static void paintText(
     Canvas canvas,
     List<Offset> points,
     String? labelText,
-    TextStyle? labelStyle,
-  ) {
-    var maxDx = 0.0;
-    var minDx = double.infinity;
-    var dx = points.map((e) => e.dx).toList().fold<double>(0.0,
-            (previousValue, element) {
-          maxDx = max(maxDx, element);
-          minDx = min(minDx, element);
-          return previousValue + element;
-        }) /
-        points.length;
-    var dy = points.map((e) => e.dy).toList().fold<double>(
-            0.0, (previousValue, element) => previousValue + element) /
-        points.length;
+    TextStyle? labelStyle, {
+    PolygonLabelPlacement labelPlacement = PolygonLabelPlacement.centroid,
+  }) {
+    late Offset placementPoint;
+    switch (labelPlacement) {
+      case PolygonLabelPlacement.centroid:
+        placementPoint = _computeCentroid(points);
+        break;
+      case PolygonLabelPlacement.polylabel:
+        placementPoint = _computePolylabel(points);
+        break;
+    }
+
+    var dx = placementPoint.dx;
+    var dy = placementPoint.dy;
 
     final textSpan = TextSpan(text: labelText, style: labelStyle);
     final textPainter = TextPainter(
@@ -31,8 +35,15 @@ class Label {
     );
     if (dx > 0) {
       textPainter.layout(minWidth: 0, maxWidth: double.infinity);
-      dx = dx - textPainter.width / 2;
-      dy = dy - textPainter.height / 2;
+      dx -= textPainter.width / 2;
+      dy -= textPainter.height / 2;
+
+      var maxDx = 0.0;
+      var minDx = double.infinity;
+      for (final point in points) {
+        maxDx = math.max(maxDx, point.dx);
+        minDx = math.min(minDx, point.dx);
+      }
 
       if (maxDx - minDx > textPainter.width) {
         textPainter.paint(
@@ -41,5 +52,22 @@ class Label {
         );
       }
     }
+  }
+
+  static Offset _computeCentroid(List<Offset> points) {
+    return Offset(
+      points.map((e) => e.dx).toList().average,
+      points.map((e) => e.dy).toList().average,
+    );
+  }
+
+  static Offset _computePolylabel(List<Offset> points) {
+    final labelPosition = polylabel([
+      points.map((p) => math.Point(p.dx, p.dy)).toList(),
+    ]);
+    return Offset(
+      labelPosition.point.x.toDouble(),
+      labelPosition.point.y.toDouble(),
+    );
   }
 }
