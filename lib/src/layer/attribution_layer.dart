75cef5412c0c7d306183dfe090792f1ee4abe0e3
Ian Wagner <ian.wagner@stadiamaps.com>
2022-10-11 01:45:43 +0900

    Initial attribution improvement

--- lib/src/layer/attribution_layer.dart
+++ lib/src/layer/attribution_layer.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/widgets.dart';
 
+class AttributionSource {
+  final String name;
+  final void Function()? onSourceTapped;
+
+  const AttributionSource({required this.name, this.onSourceTapped});
+}
+
 /// Attribution widget layer, usually placed in `nonRotatedChildren`
 ///
 /// Can be anchored in a position of the map using [alignment], defaulting to [Alignment.bottomRight]. Then pass [attributionBuilder] to build your custom attribution widget.
@@ -27,7 +34,7 @@ class AttributionWidget extends StatelessWidget {
   Widget build(BuildContext context) =>
       Align(alignment: alignment, child: attributionBuilder(context));
 
-  /// Quick constructor for a more classic styled attibution box
+  /// Quick constructor for a classic styled attribution box with a single source.
   ///
   /// Displayed as a padded translucent white box with the following text: 'flutter_map | © [source]'.
   ///
@@ -61,6 +68,44 @@ class AttributionWidget extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+          ),
+        ),
+      );
+
+  /// Quick constructor for a classic styled attribution box with multiple sources.
+  ///
+  /// Displayed as a padded translucent white box with the following text: 'flutter_map | © Attribution 1 © Attribution 2 ...'.
+  ///
+  /// Provide [onSourceTapped] to carry out a function when the box is tapped. If that isn't null, the source text will have [sourceTextStyle] styling - which defaults to a link styling.
+  static Widget defaultMultiSourceWidget({
+    required List<AttributionSource> attributions,
+    TextStyle sourceTextStyle = const TextStyle(color: Color(0xFF0078a8)),
+    Alignment alignment = Alignment.bottomRight,
+  }) =>
+      Align(
+        alignment: alignment,
+        child: ColoredBox(
+          color: const Color(0xCCFFFFFF),
+          child: Padding(
+            padding: const EdgeInsets.all(3),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: attributions
+                  .map((attr) => GestureDetector(
+                      onTap: attr.onSourceTapped,
+                      child: MouseRegion(
+                        cursor: attr.onSourceTapped == null
+                            ? MouseCursor.defer
+                            : SystemMouseCursors.click,
+                        child: Text(
+                          "© ${attr.name} ",
+                          style: attr.onSourceTapped == null
+                              ? null
+                              : sourceTextStyle,
+                        ),
+                      )))
+                  .toList(),
             ),
           ),
         ),
