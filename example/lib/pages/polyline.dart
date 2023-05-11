5c542edead9921478755713384630abb1ffdf6a6
Sebastian <sjeltsch@gmail.com>
2023-05-11 22:50:03 +0200

    Remove saveLayers from the public PolygonLayer API and call canvas.saveLayer only when needed based on the polygon properties.

--- example/lib/pages/polyline.dart
+++ example/lib/pages/polyline.dart
@@ -16,15 +16,50 @@ class _PolylinePageState extends State<PolylinePage> {
   late Future<List<Polyline>> polylines;
 
   Future<List<Polyline>> getPolylines() async {
-    final polyLines = [
+    final polyLines = <Polyline>[
       Polyline(
         points: [
           LatLng(50.5, -0.09),
           LatLng(51.3498, -6.2603),
           LatLng(53.8566, 2.3522),
         ],
-        strokeWidth: 50,
+        strokeWidth: 20,
+        color: Colors.blue.withOpacity(0.6),
+        borderStrokeWidth: 20,
+        borderColor: Colors.red.withOpacity(0.4),
+      ),
+      Polyline(
+        points: [
+          LatLng(50.2, -0.08),
+          LatLng(51.2498, -7.2603),
+          LatLng(54.8566, 1.3522),
+        ],
+        strokeWidth: 20,
+        color: Colors.black.withOpacity(0.2),
+        borderStrokeWidth: 20,
+        borderColor: Colors.white30,
+      ),
+      Polyline(
+        points: [
+          LatLng(49.1, -0.06),
+          LatLng(51.15, -7.4),
+          LatLng(55.5, 0.8),
+        ],
+        strokeWidth: 10,
+        color: Colors.yellow,
+        borderStrokeWidth: 10,
+        borderColor: Colors.blue.withOpacity(0.5),
+      ),
+      Polyline(
+        points: [
+          LatLng(48.1, -0.03),
+          LatLng(50.5, -7.8),
+          LatLng(56.5, 0.4),
+        ],
+        strokeWidth: 10,
         color: Colors.amber,
+        borderStrokeWidth: 10,
+        borderColor: Colors.blue.withOpacity(0.5),
       ),
     ];
     await Future<void>.delayed(const Duration(seconds: 3));
