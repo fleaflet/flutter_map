3a58ee8354ff119a5168f94eef54aa1ac6a2f373
josxha <34318751+josxha@users.noreply.github.com>
2023-05-15 10:12:21 +0200

    resolves https://github.com/fleaflet/flutter_map/pull/1517#discussion_r1193159151

--- lib/src/layer/tile_layer/tile_bounds/tile_bounds.dart
+++ lib/src/layer/tile_layer/tile_bounds/tile_bounds.dart
@@ -124,27 +124,23 @@ class WrappedTileBounds extends TileBounds {
     final tzDouble = zoom.toDouble();
 
     (int, int)? wrapX;
-    if (crs.wrapLng != null) {
+    if (crs.wrapLng case final wrapLng?) {
       final wrapXMin =
-          (crs.latLngToPoint(LatLng(0, crs.wrapLng!.$1), tzDouble).x /
-                  _tileSize)
+          (crs.latLngToPoint(LatLng(0, wrapLng.$1), tzDouble).x / _tileSize)
               .floor();
       final wrapXMax =
-          (crs.latLngToPoint(LatLng(0, crs.wrapLng!.$2), tzDouble).x /
-                  _tileSize)
+          (crs.latLngToPoint(LatLng(0, wrapLng.$2), tzDouble).x / _tileSize)
               .ceil();
       wrapX = (wrapXMin, wrapXMax - 1);
     }
 
     (int, int)? wrapY;
-    if (crs.wrapLat != null) {
+    if (crs.wrapLat case final wrapLat?) {
       final wrapYMin =
-          (crs.latLngToPoint(LatLng(crs.wrapLat!.$1, 0), tzDouble).y /
-                  _tileSize)
+          (crs.latLngToPoint(LatLng(wrapLat.$1, 0), tzDouble).y / _tileSize)
               .floor();
       final wrapYMax =
-          (crs.latLngToPoint(LatLng(crs.wrapLat!.$2, 0), tzDouble).y /
-                  _tileSize)
+          (crs.latLngToPoint(LatLng(wrapLat.$2, 0), tzDouble).y / _tileSize)
               .ceil();
       wrapY = (wrapYMin, wrapYMax - 1);
     }
