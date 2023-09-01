7e4c1b73caa73bc4a765467f89c67f2d3022ba86
JaffaKetchup <github@jaffaketchup.dev>
2023-09-01 11:24:55 +0100

    Prevent caching/equality if `fallbackUrl` is non-null Improved documentation

--- lib/src/layer/tile_layer/tile_provider/network_image_provider.dart
+++ lib/src/layer/tile_layer/tile_provider/network_image_provider.dart
@@ -8,6 +8,8 @@ import 'package:http/http.dart';
 /// Dedicated [ImageProvider] to fetch tiles from the network
 ///
 /// Supports falling back to a secondary URL, if the primary URL fetch fails.
+/// Note that specifying a [fallbackUrl] will prevent this image provider from
+/// being cached.
 @immutable
 class FlutterMapNetworkImageProvider
     extends ImageProvider<FlutterMapNetworkImageProvider> {
@@ -16,21 +18,27 @@ class FlutterMapNetworkImageProvider
 
   /// The URL to fetch the tile from (GET request), in the event the original
   /// [url] request fails
+  ///
+  /// If this is non-null, [operator==] will always return `false` (except if
+  /// the two objects are [identical]). Therefore, if this is non-null, this
+  /// image provider will not be cached in memory.
   final String? fallbackUrl;
 
   /// The HTTP client to use to make network requests
   ///
-  /// Not included in [operator==] calculations.
+  /// Not included in [operator==].
   final BaseClient httpClient;
 
   /// The headers to include with the tile fetch request
   ///
-  /// Not included in [operator==] calculations.
+  /// Not included in [operator==].
   final Map<String, String> headers;
 
   /// Create a dedicated [ImageProvider] to fetch tiles from the network
   ///
   /// Supports falling back to a secondary URL, if the primary URL fetch fails.
+  /// Note that specifying a [fallbackUrl] will prevent this image provider from
+  /// being cached.
   const FlutterMapNetworkImageProvider({
     required this.url,
     required this.fallbackUrl,
@@ -87,10 +95,11 @@ class FlutterMapNetworkImageProvider
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is FlutterMapNetworkImageProvider &&
-          url == other.url &&
-          fallbackUrl == other.fallbackUrl;
+      (other is FlutterMapNetworkImageProvider &&
+          fallbackUrl == null &&
+          url == other.url);
 
   @override
-  int get hashCode => Object.hash(url, fallbackUrl);
+  int get hashCode =>
+      Object.hashAll([url, if (fallbackUrl != null) fallbackUrl]);
 }
