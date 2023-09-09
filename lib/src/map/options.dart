fb7ece9efe20b0658390d1720edca0c374dc7966
Dmitry Semenov <lonelyteapot@gmail.com>
2023-09-09 15:09:00 +0300

    Add backgroundColor to operators

--- lib/src/map/options.dart
+++ lib/src/map/options.dart
@@ -304,6 +304,7 @@ class MapOptions {
       boundsOptions == other.boundsOptions &&
       minZoom == other.minZoom &&
       maxZoom == other.maxZoom &&
+      backgroundColor == other.backgroundColor &&
       onTap == other.onTap &&
       onSecondaryTap == other.onSecondaryTap &&
       onLongPress == other.onLongPress &&
@@ -330,6 +331,7 @@ class MapOptions {
         boundsOptions,
         minZoom,
         maxZoom,
+        backgroundColor,
         onTap,
         onSecondaryTap,
         onLongPress,
