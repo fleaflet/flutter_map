5a7bf1085d1c65fb6027973adbef67a3370ba1c4
Oliver Sõro <oliver.s6ro@gmail.com>
2023-09-10 12:06:44 +0300

    Fix crash on uncaught "Invalid image data" exception in case of zero bytes (MacOS desktop).
    When running a custom tile server, in the case of a missing tile we can get
    back zero bytes. Resulting in the **decode** function failure in
    **_loadAsync** of FlutterMapNetworkImageProvider.
    
    Looking at the **_loadAsync** implementation, the call to the **decode**
    function was left out of the try/catch block. Simply moving the
    **decode** function call inside the try block fixes the issue - the app
    no longer crashes on a missing tile.

--- lib/src/layer/tile_layer/tile_provider/network_image_provider.dart
+++ lib/src/layer/tile_layer/tile_provider/network_image_provider.dart
@@ -84,12 +84,12 @@ class FlutterMapNetworkImageProvider
         Uri.parse(useFallback ? fallbackUrl ?? '' : url),
         headers: headers,
       );
+
+      return decode(await ImmutableBuffer.fromUint8List(bytes));
     } catch (_) {
       if (useFallback || fallbackUrl == null) rethrow;
       return _loadAsync(key, chunkEvents, decode, useFallback: true);
     }
-
-    return decode(await ImmutableBuffer.fromUint8List(bytes));
   }
 
   @override
