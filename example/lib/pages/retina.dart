6af61d48c5ac3af407fb4d3f7a5a1eda11fa8d44
Andrew Brampton <github@bramp.net>
2023-09-28 15:19:40 -0700

    Updated the Retina Example page based on the new retinaMethod API.

--- example/lib/pages/retina.dart
+++ example/lib/pages/retina.dart
@@ -7,6 +7,12 @@ import 'package:url_launcher/url_launcher.dart';
 class RetinaPage extends StatefulWidget {
   static const String route = '/retina';
 
+  final String defaultUrlTemplate =
+      'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}{r}?access_token={accessToken}';
+  // TODO Remove
+  final String defaultAccessToken =
+      'pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2p0MG01MXRqMW45cjQzb2R6b2ptc3J4MSJ9.zA2W0IkI0c6KaAhJfk9bWg';
+
   const RetinaPage({Key? key}) : super(key: key);
 
   @override
@@ -14,10 +20,19 @@ class RetinaPage extends StatefulWidget {
 }
 
 class _RetinaPageState extends State<RetinaPage> {
-  bool fmRetina = false;
-  bool urlRetina = false;
-  double fmTileSize = 256;
-  double urlTileSize = 256;
+  bool retina = false;
+  RetinaMethod retinaMethod = RetinaMethod.auto;
+
+  late String urlTemplate;
+  late String accessToken;
+
+  @override
+  void initState() {
+    super.initState();
+
+    urlTemplate = widget.defaultUrlTemplate;
+    accessToken = widget.defaultAccessToken;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,83 +43,102 @@ class _RetinaPageState extends State<RetinaPage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(8),
-            child: IntrinsicHeight(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      const Text(
-                        'flutter_map Settings',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                OverflowBar(
+                  spacing: 8,
+                  overflowAlignment: OverflowBarAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text('Retina'),
+                        const SizedBox.square(dimension: 10),
+                        Switch.adaptive(
+                          value: retina,
+                          onChanged: (v) => setState(() => retina = v),
+                        ),
+                        DropdownMenu(
+                          label: const Text('Retina Method'),
+                          initialSelection: RetinaMethod.auto,
+                          enableSearch: false,
+                          enabled: retina,
+                          dropdownMenuEntries: const [
+                            DropdownMenuEntry(
+                                label: 'auto', value: RetinaMethod.auto),
+                            DropdownMenuEntry(
+                                label: 'server', value: RetinaMethod.server),
+                            DropdownMenuEntry(
+                                label: 'simulate',
+                                value: RetinaMethod.simulate),
+                          ],
+                          onSelected: (v) {
+                            if (v == null) return;
+                            setState(() => retinaMethod = v);
+                          },
+                        ),
+                      ],
+                    ),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        minWidth: 0,
+                        maxWidth: 400,
+                        minHeight: 0,
+                        maxHeight: double.infinity,
                       ),
-                      const SizedBox.square(dimension: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                      child: Column(
                         children: [
-                          const Text('Simulate Retina'),
-                          const SizedBox.square(dimension: 10),
-                          Switch.adaptive(
-                            value: fmRetina,
-                            onChanged: (v) => setState(() => fmRetina = v),
+                          TextFormField(
+                            initialValue: widget.defaultUrlTemplate,
+                            onChanged: (v) => setState(() => urlTemplate = v),
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.link),
+                              border: UnderlineInputBorder(),
+                              labelText: 'URL Template',
+                            ),
+                          ),
+                          TextFormField(
+                            initialValue: widget.defaultAccessToken,
+                            onChanged: (v) => setState(() => accessToken = v),
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.lock),
+                              border: const UnderlineInputBorder(),
+                              labelText: 'Access Token',
+                              errorText: accessToken.isEmpty
+                                  ? "Please request a token from https://docs.mapbox.com/help/glossary/access-token/"
+                                  : null,
+                            ),
                           ),
                         ],
                       ),
-                      const SizedBox.square(dimension: 10),
-                      DropdownMenu<double>(
-                        label: const Text('Tile Size'),
-                        leadingIcon: const Icon(Icons.photo_size_select_large),
-                        initialSelection: 256,
-                        enableSearch: false,
-                        dropdownMenuEntries: const [
-                          DropdownMenuEntry(label: '256px', value: 256),
-                          DropdownMenuEntry(label: '512px', value: 512),
-                        ],
-                        onSelected: (tileSize) {
-                          if (tileSize == null) return;
-                          setState(() => fmTileSize = tileSize);
-                        },
-                      ),
-                    ],
-                  ),
-                  const VerticalDivider(width: 42),
-                  Column(
-                    children: [
-                      const Text(
-                        'URL Options',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox.square(dimension: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('"@2x" Retina'),
-                          const SizedBox.square(dimension: 10),
-                          Switch.adaptive(
-                            value: urlRetina,
-                            onChanged: (v) => setState(() => urlRetina = v),
-                          ),
-                        ],
-                      ),
-                      const SizedBox.square(dimension: 10),
-                      DropdownMenu<double>(
-                        label: const Text('Tile Size'),
-                        leadingIcon: const Icon(Icons.photo_size_select_large),
-                        initialSelection: 256,
-                        enableSearch: false,
-                        dropdownMenuEntries: const [
-                          DropdownMenuEntry(label: '256px', value: 256),
-                          DropdownMenuEntry(label: '512px', value: 512),
-                        ],
-                        onSelected: (tileSize) {
-                          if (tileSize == null) return;
-                          setState(() => urlTileSize = tileSize);
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+                const SizedBox.square(dimension: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Screen Density: ',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                        '@${MediaQuery.of(context).devicePixelRatio.toStringAsFixed(2)}'
+                        'x'),
+                    const SizedBox.square(dimension: 10),
+                    const Text(
+                      'Chosen Method: ',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(retina
+                        // ignore: invalid_use_of_visible_for_testing_member
+                        ? TileLayer.determineRetinaMethod(
+                                retinaMethod, urlTemplate)
+                            .toString()
+                        : 'Disabled'),
+                  ],
+                )
+              ],
             ),
           ),
           Expanded(
@@ -112,6 +146,7 @@ class _RetinaPageState extends State<RetinaPage> {
               options: const MapOptions(
                 initialCenter: LatLng(51.5, -0.09),
                 initialZoom: 5,
+                maxZoom: 19,
               ),
               nonRotatedChildren: [
                 RichAttributionWidget(
@@ -145,26 +180,24 @@ class _RetinaPageState extends State<RetinaPage> {
                 ),
               ],
               children: [
-                TileLayer(
-                  urlTemplate:
-                      'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{urlTileSize}/{z}/{x}/{y}{urlRetinaMode}?access_token={accessToken}',
-                  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-                  additionalOptions: {
-                    'urlTileSize': urlTileSize.toStringAsFixed(0),
-                    'urlRetinaMode': urlRetina ? '@2x' : '',
-                    'accessToken':
-                        'pk.eyJ1IjoiYnJhbXAiLCJhIjoiY2o4b3NnamZnMDhoMjMzcno4M3JrNm4wMSJ9.R8ove2k7wvRVVdEnlTUu4A',
-                  },
-                  retinaMode: fmRetina,
-                  tileSize: fmTileSize,
-                  tileBuilder: (context, tileWidget, _) => DecoratedBox(
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 2, color: Colors.white),
+                if (accessToken.isNotEmpty)
+                  TileLayer(
+                    urlTemplate: urlTemplate,
+                    userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                    additionalOptions: {
+                      'accessToken': accessToken,
+                    },
+                    retinaMode: retina,
+                    retinaMethod: retinaMethod,
+                    tileSize: 256,
+                    tileBuilder: (context, tileWidget, _) => DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 2, color: Colors.white),
+                      ),
+                      position: DecorationPosition.foreground,
+                      child: tileWidget,
                     ),
-                    position: DecorationPosition.foreground,
-                    child: tileWidget,
-                  ),
-                )
+                  )
               ],
             ),
           ),
