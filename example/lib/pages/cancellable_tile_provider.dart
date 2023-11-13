0af46c5f397b0fabc03c29dc01d2d97be27d89a4
Guillaume Roux <rouxguillaume8@gmail.com>
2023-11-13 18:42:43 +0100

    feat: export 'latlong2' (#1731)

--- example/lib/pages/cancellable_tile_provider.dart
+++ example/lib/pages/cancellable_tile_provider.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:flutter_map_example/widgets/drawer.dart';
-import 'package:latlong2/latlong.dart';
 
 class CancellableTileProviderPage extends StatelessWidget {
   static const String route = '/cancellable_tile_provider_page';
--- example/lib/pages/wms_tile_layer.dart
+++ example/lib/pages/wms_tile_layer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_example/widgets/drawer.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class WMSLayerPage extends StatelessWidget {
