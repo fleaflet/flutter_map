5b8e27ea7fc44fd878a7f9428e3c8d6fd01b720a
Joscha <34318751+josxha@users.noreply.github.com>
2023-11-22 22:12:26 +0100

    create `FirstStartDialog` widget

--- example/lib/pages/home.dart
+++ example/lib/pages/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_example/widgets/drawer/menu_drawer.dart';
+import 'package:flutter_map_example/widgets/first_start_dialog.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -19,80 +20,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-
-    const seenIntroBoxKey = 'seenIntroBox(a)';
-    if (kIsWeb && Uri.base.host.trim() == 'demo.fleaflet.dev') {
-      SchedulerBinding.instance.addPostFrameCallback(
-        (_) async {
-          final prefs = await SharedPreferences.getInstance();
-          if (prefs.getBool(seenIntroBoxKey) ?? false) return;
-
-          if (!mounted) return;
-
-          final width = MediaQuery.of(context).size.width;
-          await showDialog<void>(
-            context: context,
-            builder: (context) => AlertDialog(
-              icon: UnconstrainedBox(
-                child: SizedBox.square(
-                  dimension: 64,
-                  child:
-                      Image.asset('assets/ProjectIcon.png', fit: BoxFit.fill),
-                ),
-              ),
-              title: const Text('flutter_map Live Web Demo'),
-              content: ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxWidth: width < 750
-                      ? double.infinity
-                      : (width / (width < 1100 ? 1.5 : 2.5)),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text(
-                      "This is built automatically off of the latest commits to 'master', so may not reflect the latest release available on pub.dev.\nThis is hosted on Firebase Hosting, meaning there's limited bandwidth to share between all users, so please keep loads to a minimum.",
-                      textAlign: TextAlign.center,
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(right: 8, top: 16, bottom: 4),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "This won't be shown again",
-                          style: TextStyle(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .inverseSurface
-                                .withOpacity(0.5),
-                          ),
-                          textAlign: TextAlign.right,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              actions: [
-                TextButton.icon(
-                  onPressed: () => Navigator.of(context).pop(),
-                  label: const Text('OK'),
-                  icon: const Icon(Icons.done),
-                ),
-              ],
-              contentPadding: const EdgeInsets.only(
-                left: 24,
-                top: 16,
-                bottom: 0,
-                right: 24,
-              ),
-            ),
-          );
-          await prefs.setBool(seenIntroBoxKey, true);
-        },
-      );
-    }
+    showIntroDialogIfNeeded();
   }
 
   @override
@@ -166,5 +94,25 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  void showIntroDialogIfNeeded() {
+    const seenIntroBoxKey = 'seenIntroBox(a)';
+    if (kIsWeb && Uri.base.host.trim() == 'demo.fleaflet.dev') {
+      SchedulerBinding.instance.addPostFrameCallback(
+        (_) async {
+          final prefs = await SharedPreferences.getInstance();
+          if (prefs.getBool(seenIntroBoxKey) ?? false) return;
+
+          if (!mounted) return;
+
+          await showDialog<void>(
+            context: context,
+            builder: (context) => const FirstStartDialog(),
+          );
+          await prefs.setBool(seenIntroBoxKey, true);
+        },
+      );
+    }
   }
 }
