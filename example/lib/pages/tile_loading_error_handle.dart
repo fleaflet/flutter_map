3251839f9c24c7bbe21aa1c3827d391a5f81b501
Joscha <34318751+josxha@users.noreply.github.com>
2023-11-29 00:49:20 +0100

    fix: warnings and more from #1730

--- example/lib/pages/tile_loading_error_handle.dart
+++ example/lib/pages/tile_loading_error_handle.dart
@@ -1,6 +1,3 @@
-import 'dart:ui' as ui;
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
@@ -103,14 +100,9 @@ class _SimulateErrorImageProvider
   _SimulateErrorImageProvider();
 
   @override
-  ImageStreamCompleter load(
+  ImageStreamCompleter loadImage(
     _SimulateErrorImageProvider key,
-    Future<ui.Codec> Function(
-      Uint8List, {
-      bool allowUpscaling,
-      int? cacheHeight,
-      int? cacheWidth,
-    }) decode,
+    ImageDecoderCallback decode,
   ) =>
       _SimulateErrorImageStreamCompleter();
 
