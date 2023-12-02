61154cde401bd705fe58ede6f5d67f737ef0712e
JaffaKetchup <github@jaffaketchup.dev>
2023-12-02 11:15:49 +0000

    Revert #1731

--- example/lib/pages/scalebar_utils.dart
+++ example/lib/pages/scalebar_utils.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 import 'dart:ui';
 
-import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 const double piOver180 = pi / 180.0;
 
--- example/lib/plugins/plugin_scalebar.dart
+++ example/lib/plugins/plugin_scalebar.dart
@@ -3,6 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_example/misc/tile_providers.dart';
 import 'package:flutter_map_example/pages/scale_layer_plugin_option.dart';
 import 'package:flutter_map_example/widgets/drawer/menu_drawer.dart';
+import 'package:latlong2/latlong.dart';
 
 class PluginScaleBar extends StatelessWidget {
   static const String route = '/plugin_scalebar';
