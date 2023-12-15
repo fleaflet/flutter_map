9f1d4a13e89c4daaae81e4762635fe3f48d7da8c
Joscha <34318751+josxha@users.noreply.github.com>
2023-12-15 21:40:12 +0100

    add docs, add ctrlRotateKeys to the hashKey and == function

--- lib/src/map/options/interaction_options.dart
+++ lib/src/map/options/interaction_options.dart
@@ -4,7 +4,17 @@ import 'package:meta/meta.dart';
 
 @immutable
 final class InteractionOptions {
-  /// See [EnabledGestures] for custom settings
+  /// Enable or disable specific gestures. By default all gestures are enabled.
+  /// If you want to disable all gestures or almost all gestures, use the
+  /// [EnabledGestures.none] constructor.
+  /// In case you want to disable only few gestures, use [EnabledGestures.all]
+  /// and you can use the [EnabledGestures.noRotation] for an easy way to
+  /// disable all rotation gestures.
+  ///
+  /// In addition you can specify your gestures via bitfield operations using
+  /// the [EnabledGestures.bitfield] constructor together with the static
+  /// fields in [InteractiveFlag].
+  /// For more information see the documentation on [InteractiveFlag].
   final EnabledGestures enabledGestures;
 
   /// Rotation threshold in degree default is 20.0 Map starts to rotate when
@@ -12,14 +22,14 @@ final class InteractionOptions {
   /// which allows [MultiFingerGesture.rotate] Note: if [interactiveFlags]
   /// doesn't contain [InteractiveFlag.rotate] or [enableMultiFingerGestureRace]
   /// is false then rotate cannot win
-  final double rotationThreshold;
+  final double rotationThreshold; // TODO
 
   /// Pinch Zoom threshold default is 0.5 Map starts to zoom when
   /// [pinchZoomThreshold] has been achieved or another multi finger gesture
   /// wins which allows [MultiFingerGesture.pinchZoom] Note: if
   /// [interactiveFlags] doesn't contain [InteractiveFlag.pinchZoom] or
   /// [enableMultiFingerGestureRace] is false then zoom cannot win
-  final double pinchZoomThreshold;
+  final double pinchZoomThreshold; // TODO
 
   /// Pinch Move threshold default is 40.0 (note: this doesn't take any effect
   /// on drag) Map starts to move when [pinchMoveThreshold] has been achieved or
@@ -27,10 +37,12 @@ final class InteractionOptions {
   /// [MultiFingerGesture.pinchMove] Note: if [interactiveFlags] doesn't contain
   /// [InteractiveFlag.pinchMove] or [enableMultiFingerGestureRace] is false
   /// then pinch move cannot win
-  final double pinchMoveThreshold;
+  final double pinchMoveThreshold; // TODO
 
-  final double scrollWheelVelocity;
+  final double scrollWheelVelocity; // TODO
 
+  /// Override this option if you want to use custom keys for the CTRL+drag
+  /// rotate gesture. By default the left and right control key are used.
   final List<LogicalKeyboardKey> ctrlRotateKeys;
 
   const InteractionOptions({
@@ -65,6 +77,7 @@ final class InteractionOptions {
           rotationThreshold == other.rotationThreshold &&
           pinchZoomThreshold == other.pinchZoomThreshold &&
           pinchMoveThreshold == other.pinchMoveThreshold &&
+          ctrlRotateKeys == other.ctrlRotateKeys &&
           scrollWheelVelocity == other.scrollWheelVelocity);
 
   @override
@@ -73,6 +86,7 @@ final class InteractionOptions {
         rotationThreshold,
         pinchZoomThreshold,
         pinchMoveThreshold,
+        ctrlRotateKeys,
         scrollWheelVelocity,
       );
 }
