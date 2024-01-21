3b4fedb7f8ecb994d2ec46f5cc279de925d9ed0d
Joscha <34318751+josxha@users.noreply.github.com>
2024-01-21 23:18:43 +0100

    import from flutter_map library where possible

--- test/geo/latlng_bounds_test.dart
+++ test/geo/latlng_bounds_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_map/src/geo/latlng_bounds.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:latlong2/latlong.dart';
 
