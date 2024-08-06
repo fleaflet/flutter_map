415085f34c2aecbff55e498b1e85a1514cec8b3d
JaffaKetchup <github@jaffaketchup.dev>
2024-08-06 22:55:35 +0200

    Fixed lint issue & added TODO

--- lib/src/layer/polygon_layer/painter.dart
+++ lib/src/layer/polygon_layer/painter.dart
@@ -52,7 +52,12 @@ base class _PolygonPainter<R extends Object>
     required math.Point<double> point,
     required LatLng coordinate,
   }) {
-    final polygon = projectedPolygon.polygon;
+    // TODO: We should check the bounding box here, for efficiency
+    // However, we need to account for map rotation
+    //
+    // if (!polygon.boundingBox.contains(touch)) {
+    //   continue;
+    // }
 
     final projectedCoords = getOffsetsXY(
       camera: camera,
