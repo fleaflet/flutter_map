32c95f53f1bb60ab33960d18203ef6e0fa68908c
Alejandro Santiago <me@alestiago.com>
2024-10-01 11:04:21 +0100

    docs: document hasGesture

--- lib/src/misc/position.dart
+++ lib/src/misc/position.dart
@@ -9,6 +9,14 @@ class MapPosition {
   final LatLng? center;
   final LatLngBounds? bounds;
   final double? zoom;
+
+  /// {@template map_position.has_gesture}
+  /// [hasGesture] determines if the change was caused by a user gesture (interaction with the map) if true,
+  /// otherwise, if false, the change was not caused by a user gesture, but by a programmatic change
+  ///
+  /// For example, if the map's position was changed using the map's controller, then [hasGesture] would be false
+  /// because the change was not caused by a user gesture, but programmatically.
+  /// {@endtemplate}
   final bool hasGesture;
 
   const MapPosition({
@@ -28,5 +36,3 @@ class MapPosition {
       other.bounds == bounds &&
       other.zoom == zoom;
 }
-
-typedef PositionCallback = void Function(MapPosition position, bool hasGesture);
