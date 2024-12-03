bd99ca1bf34ad056307b178c876d2222a868ff63
mootw <spencer@xqkz.net>
2024-12-02 18:01:51 -0600

    deprecate notices, only really needed because of bounds and tilecoordinates

--- lib/src/misc/extensions.dart
+++ lib/src/misc/extensions.dart
@@ -3,31 +3,22 @@ import 'dart:ui';
 
 /// Extension methods for the math.[Point] class
 extension PointExtension<T extends num> on Point<T> {
-  /// Create new [Point] whose [x] and [y] values are divided by the respective
-  /// values in [point].
-  Point<double> unscaleBy(Point<num> point) {
-    return Point<double>(x / point.x, y / point.y);
-  }
-
   /// Create a new [Point] where the [x] and [y] values are divided by [factor].
+  @Deprecated('Replace with Offset')
   Point<double> operator /(num factor) {
     return Point<double>(x / factor, y / factor);
   }
 
-  /// Create a new [Point] where the [x] and [y] values are rounded to the
-  /// nearest integer.
-  Point<int> round() {
-    return Point<int>(x.round(), y.round());
-  }
-
   /// Create a new [Point] where the [x] and [y] values are rounded up to the
   /// nearest integer.
+  @Deprecated('Replace with Offset')
   Point<int> ceil() {
     return Point<int>(x.ceil(), y.ceil());
   }
 
   /// Create a new [Point] where the [x] and [y] values are rounded down to the
   /// nearest integer.
+  @Deprecated('Replace with Offset')
   Point<int> floor() {
     return Point<int>(x.floor(), y.floor());
   }
