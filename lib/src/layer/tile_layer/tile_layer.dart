34587cc02024628e5b4d4a5ded10d40f5539250b
JaffaKetchup <github@jaffaketchup.dev>
2025-03-23 11:04:12 +0000

    Minor formatting fix

--- lib/src/layer/tile_layer/tile_layer.dart
+++ lib/src/layer/tile_layer/tile_layer.dart
@@ -18,9 +18,7 @@ import 'package:http/retry.dart';
 import 'package:logger/logger.dart';
 
 part 'retina_mode.dart';
-
 part 'tile_error_evict_callback.dart';
-
 part 'wms_tile_layer_options.dart';
 
 /// Describes the needed properties to create a tile-based layer. A tile is an
