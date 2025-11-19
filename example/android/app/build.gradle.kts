c63207510181623eff354dceb6b5e460f6437101
Luka S <github@jaffaketchup.dev>
2025-11-19 14:29:27 +0000

    Remove old `ndkVersion`
    Co-authored-by: Joscha Eckert <34318751+josxha@users.noreply.github.com>

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -9,7 +9,6 @@ android {
     namespace = "dev.fleaflet.flutter_map.example"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-    // ndkVersion = "27.0.12077973" (?)
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
