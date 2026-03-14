9d00de65e6f5365d3a0b6276f20ec36d4db899fa
monsieurtanuki <fabrice_fontaine@hotmail.com>
2026-03-14 13:34:45 +0100

    chore: absorb necessary 'dart:io' components to remove references (#2186)

--- lib/src/layer/tile_layer/tile_provider/network/caching/tile_metadata.dart
+++ lib/src/layer/tile_layer/tile_provider/network/caching/tile_metadata.dart
@@ -1,8 +1,9 @@
-import 'dart:io' show HttpHeaders, HttpDate; // web safe!
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map/src/dart_io/http_date.dart';
+import 'package:flutter_map/src/dart_io/http_headers.dart';
 
 /// Metadata about a tile cached with a [MapCachingProvider]
 ///
