db13118216417d3fc657682dd65f0265d29cbc23
JaffaKetchup <github@jaffaketchup.dev>
2026-09-03 18:17:32 +0100

    Convert most 'package:flutter/material.dart' imports to 'widgets.dart'

--- lib/src/map/controller/map_controller.dart
+++ lib/src/map/controller/map_controller.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/src/map/inherited_model.dart';
 import 'package:flutter_map/src/misc/move_and_rotate_result.dart';
