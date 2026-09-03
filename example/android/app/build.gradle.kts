6b203889c7c0674d77f079b2d41aabe725f12a3e
JaffaKetchup <github@jaffaketchup.dev>
2026-09-03 17:58:56 +0100

    Undo upgrade to built-in Kotlin

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -14,6 +15,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
     defaultConfig {
         applicationId = "dev.fleaflet.flutter_map.example"
         minSdk = flutter.minSdkVersion
@@ -26,12 +31,6 @@ android {
         release {
             signingConfig = signingConfigs.getByName("debug")
         }
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
